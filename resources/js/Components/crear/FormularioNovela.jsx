5aff90b10565da9569ee3051b7fa489c81247d5e
diegoghernandez <diegoj4v@gmail.com>
2024-10-22 19:33:36 -0600

    Agregar componente FormularioNovela para obtener los datos

---
+++ resources/js/Components/crear/FormularioNovela.jsx
@@ -0,0 +1,51 @@
+import { CartaImagenIA } from "@/Components/common/CartaImagenIA";
+import { CloudinaryWidget } from "@/Components/common/CloudinaryWidget";
+import { CustomInput } from "@/Components/common/CustomInput";
+import { useCrear } from "@/Hooks/useCrear";
+import { useForm } from "@inertiajs/react";
+
+export function FormularioNovela() {
+	const agregarNovela = useCrear((state) => state.agregarNovela);
+	const { data, setData } = useForm({
+		titulo: "",
+		descripcion: "",
+		imagenUrl: "",
+	});
+
+	const obtenerImagenUrl = (url) => {
+		setData("imagenUrl", url);
+	};
+
+	const onSubmit = (event) => {
+		event.preventDefault();
+
+		agregarNovela(data);
+	};
+
+	return (
+		<CartaImagenIA obtenerImagen={obtenerImagenUrl}>
+			<form onSubmit={onSubmit}>
+				<CustomInput
+					label="Título"
+					name="titulo"
+					required={true}
+					type="text"
+					onChange={(e) => setData("titulo", e.target.value)}
+				/>
+				<CustomInput
+					label="Descripción"
+					name="descripcion"
+					required={true}
+					type="text"
+					onChange={(e) => setData("descripcion", e.target.value)}
+				/>
+				<CloudinaryWidget />
+				<button
+					disabled={!data.titulo || !data.descripcion || !data.imagenUrl}
+				>
+					Guardar
+				</button>
+			</form>
+		</CartaImagenIA>
+	);
+}
